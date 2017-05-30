764ea19ce1367932d31f7509122f790c76617f63
Dennis Sänger <mail@dennis.io>
2017-05-30 15:41:19 +0200

    fix(package.json): catch error if package.json is missing

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ interface IPackageJson {
 };
 
 export function getFiles(rootDir: string) {
-  const pack = getPackageJson(path.join(rootDir, PACKAGE_JSON));
+  const pack = tryCatch(() => getPackageJson(path.join(rootDir, PACKAGE_JSON)), () => {});
   const deps = getDependencies(pack);
 
   let styles = deps
@@ -17,17 +17,25 @@ export function getFiles(rootDir: string) {
   return flatten(styles);
 };
 
+function tryCatch(tryFn, catchFn) {
+  try {
+    return tryFn();
+  } catch (e) {
+    return catchFn(e);
+  }
+}
+
 function getPackageJson(url: string) {
   return JSON.parse(fs.readFileSync(url));
 }
 
 function getDependencies(pack: IPackageJson) {
-  return Object.keys(getField('dependencies', pack));
+  return Object.keys(getField('dependencies', pack, {}));
 }
 
-function getField(field: string, pack: IPackageJson) {
+function getField(field: string, pack: IPackageJson, fallback: any = '') {
   const hasField = (pack && pack.hasOwnProperty && pack.hasOwnProperty(field));
-  return hasField ? pack[field] : '';
+  return hasField ? pack[field] : fallback;
 }
 
 function getStyleField(dependency: string, rootDir: string) {
